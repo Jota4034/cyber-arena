8ac506e8e3cfb23064286af664a4016fedc22fdf
HENRIQUE GABRIEL VELASQUI <henrique_velasqui@estudante.sesisenai.org.br>
2026-06-23 15:10:51 -0300

    adicionado saudação

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void suadacao(){
+	cout << "Bem-vindo ao sistema!" <<endl;	
+} 
+
 int main(){
-	
+	saudacao();
 	cout << "Projeto em Grupo" << endl;
 	
 	return 0;
