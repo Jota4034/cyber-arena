5ee3170dd5e436141249f8a7acb93fdfb8adab8b
HENRIQUE GABRIEL VELASQUI <henrique_velasqui@estudante.sesisenai.org.br>
2026-06-30 15:45:40 -0300

    finalizado a simulação combate entre dois personagens na main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
+#include "batalha.h"
 using namespace std;
-
-void despedida(){
-	cout << "Até breve! " << endl;
-}
-void saudacao(){
-	cout << "Bem-vindo ao sistema!" <<endl;	
-} 
-
 int main(){
-	despedida();
-	saudacao();
-	cout << "Projeto em Grupo" << endl;
-	
-	return 0; 
+
 }
